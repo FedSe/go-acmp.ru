426ed1ec5821f3ec7729eb8451d84af47979c844
FedSe_ <cemeh.bl@ya.ru>
2025-07-26 23:02:35 +0700

    []*int выгоднее

--- V343.go
+++ V343.go
@@ -1,58 +1,47 @@
 package main
 import . "fmt"
 func main() {
-    var (
-        s [53][53]int
-        n, m, v, y, x int
-    )
-    
-    Scan(&n, &m)
-    for y < 53 {
-        for
-        x = 0
-        x < 53
-        {
-            o := 1
-            if y <= n && x <= m && x * y > 0 { o = 0 }
-            s[y][x] = o
-            x++
-        }
-        y++
-    }
+	var (
+		s             [53][53]int
+		n, m, v, y, x int
+	)
 
-    Scan(&n)
-    for n > 0 {
-        Scan(&m, &y, &x)
-        a := &s[y+1][x+1]
-        b := &s[y][x+1]
-        c := &s[y+1][x]
-        d := &s[y][x]
-        if m < 2 && *c + *b + *a < 1 {
-            *c = 1
-            *b = 1
-            *a = 1
-            v += 3
-        }
-        if m == 2 && *c + *d + *a < 1 {
-            *c = 1
-            *d = 1
-            *a = 1
-            v += 3
-        }
-        if m == 3 && *d + *b+ *a < 1 {
-            *d = 1
-            *b = 1
-            *a = 1
-            v += 3
-        }
-        if m > 3 && *d + *b + *c < 1 {
-            *d = 1
-            *b = 1
-            *c = 1
-            v += 3
-        }
-        n--
-    }
+	Scan(&n, &m)
+	for y < 53 {
+		x = 0
+		for x < 53 {
+			o := 1
+			if y <= n && x <= m && x*y > 0 {
+				o = 0
+			}
+			s[y][x] = o
+			x++
+		}
+		y++
+	}
 
-    Print(v)
+	Scan(&n)
+	for n > 0 {
+		Scan(&m, &y, &x)
+		a, b, c, d := &s[y+1][x+1], &s[y][x+1], &s[y+1][x], &s[y][x]
+		w := []*int{a, b, c}
+		if m == 2 {
+			w = []*int{a, c, d}
+		}
+		if m == 3 {
+			w = []*int{a, b, d}
+		}
+		if m > 3 {
+			w = []*int{b, c, d}
+		}
+		if *w[0]+*w[1]+*w[2] < 1 {
+			*w[0] = 1
+			*w[1] = 1
+			*w[2] = 1
+			v += 3
+		}
+		n--
+	}
+
+	Print(v)
 }

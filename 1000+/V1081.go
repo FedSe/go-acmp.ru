44baff509cec0a26922dcb128ca2169c26704210
FedSe_ <cemeh.bl@ya.ru>
2025-07-27 10:23:32 +0700

    NewReader() вместо NewScanner() снова

--- 1000+/V1081.go
+++ 1000+/V1081.go
@@ -3,18 +3,19 @@ import (
 	b "bufio"
 	. "fmt"
 	. "os"
-	. "strconv"
 )
+
 var t [400000]int
+
 func F(v, f, w int, a []int) {
 	if f == w {
 		t[v] = a[f]
 		return
 	}
 	d := f + (w-f)/2
-	F(2*v + 1, f, d, a)
-	F(2*v + 2, d+1, w, a)
-	t[v] = t[2*v + 1] + t[2*v + 2]
+	F(2*v+1, f, d, a)
+	F(2*v+2, d+1, w, a)
+	t[v] = t[2*v+1] + t[2*v+2]
 }
 func H(v, f, w, l, r int) int {
 	if r < f || l > w {
@@ -24,25 +25,24 @@ func H(v, f, w, l, r int) int {
 		return t[v]
 	}
 	d := f + (w-f)/2
-	return H(2*v + 1, f, d, l, r) + H(2*v + 2, d+1, w, l, r)
+	return H(2*v+1, f, d, l, r) + H(2*v+2, d+1, w, l, r)
 }
 func main() {
-	var n, m, l, r, i, j int
-	s := b.NewScanner(Stdin)
-	s.Split(b.ScanWords)
-	Scan(&n)
+	var n, m, l, r int
+	s := b.NewReader(Stdin)
+
+	Fscan(s, &n)
 	a := make([]int, n)
-	for j < n {
-		Scan(&a[j])
-	j++
+	for m < n {
+		Fscan(s, &a[m])
+		m++
 	}
-	F(0, 0, n-1, a)
-	Scan(&m)
-	for s.Scan() {
-		l, _ = Atoi(s.Text())
-		s.Scan()
-		r, _ = Atoi(s.Text())
-		Println(H(0, 0, n-1, l-1, r-1))
-	i++
+	n--
+	F(0, 0, n, a)
+	Fscan(s, &m)
+	for m > 0 {
+		Fscan(s, &l, &r)
+		Println(H(0, 0, n, l-1, r-1))
+		m--
 	}
 }

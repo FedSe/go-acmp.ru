727686b3955179da98e57ba4a249b2173e8e0aef
FedSe_ <cemeh.bl@ya.ru>
2025-07-27 17:09:02 +0700

    11# Поиск композиций числа N

--- V11.go
+++ V11.go
@@ -1,39 +1,25 @@
 package main
-import . "fmt"
+import (
+	. "fmt"
+	. "math/big"
+)
 func main() {
 	var (
-		m [300][99]int
 		k, n, i int
+		d       [301]*Int
 	)
+	d[0] = NewInt(1)
 
 	Scan(&k, &n)
 	for i < n {
-		if i < k {
-			m[i][0] = 1
+		i++
+		d[i] = NewInt(0)
+		j := 1
+		for j <= k && j <= i {
+			d[i].Add(d[i], d[i-j])
+			j++
 		}
-		j := i-k
-		if j < 0 { j = 0 }
-		for j < i {
-			t := 0
-			w := 0
-			for t < 99 {
-				w = m[i][t] + m[j][t] + w / 10
-				m[i][t] = w % 10
-			t++
-			}
-		j++
-		}
-	i++
 	}
 
-	n--
-	i = 98
-	for m[n][i] < 1 {
-		i--
-	}
-
-	for i >= 0 {
-		Print(m[n][i])
-	i--
-	}
+	Print(d[n])
 }

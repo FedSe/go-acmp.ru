b64fb0256d0e707e994b412ed6f260f870886d8c
FedSe_ <cemeh.bl@ya.ru>
2025-07-28 23:31:58 +0700

    1087# string([]byte{}) выгоднее

--- 1000+/V1087.go
+++ 1000+/V1087.go
@@ -6,13 +6,14 @@ func main() {
 	b := s
 
 	Scan(&a, &b)
-
-	if (a[0] == 112 && b[0] == 114) || (a[0] == 114 && b[0] == 115) || (a[0] == 115 && b[0] == 112) {
-		s = "first"
-	}
-
+	
 	if a == b {
 		s = "draw"
+	}
+    
+	a = string([]byte{a[0], b[0]})
+	if a == "pr" || a == "rs" || a == "sp" {
+		s = "first"
 	}
 
 	Print(s)
